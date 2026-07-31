26daebe3064208b2104b5b9539ff4a47b215f0a5
argonptg <argonptg@gmail.com>
2026-07-31 10:51:24 -0300

    Create chocolatey package

---
+++ Packages/Chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/Y0URD34TH/Project-GLD/releases/download/Update-V7.35/GLDSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url64bit      = $url64
+
+  softwareName  = 'project-gld*'
+
+  checksum64    = '62a82832557e9b4e440cb087059afcf8451c8b15905e0985fab9aa48738c953d'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
